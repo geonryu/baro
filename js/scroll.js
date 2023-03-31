7d2b280019d9838fc90345d5aa95273eb70b1e4c
유건 <geon@yugeon-ui-Macmini.local>
2023-03-31 10:27:18 +0900

    test

--- js/scroll.js
+++ js/scroll.js
@@ -1,41 +1,46 @@
 gsap.registerPlugin(ScrollTrigger);
 
-let getRatio = el => window.innerHeight / (window.innerHeight + el.offsetHeight);
+let getRatio = (el) =>
+  window.innerHeight / (window.innerHeight + el.offsetHeight);
 
 gsap.utils.toArray(".parallxBg").forEach((section, i) => {
-
-  gsap.fromTo(section, {
-    backgroundPosition: () => i ? `50% ${-window.innerHeight * getRatio(section)}px` : "50% 0px"
-  }, {
-    backgroundPosition: () => `50% ${window.innerHeight * (1 - getRatio(section))}px`,
-    ease: "none",
-    scrollTrigger: {
-      trigger: section,
-      start: () => i ? "top bottom" : "top top", 
-      end: "bottom top",
-      scrub: true,
-      invalidateOnRefresh: true // to make it responsive
+  gsap.fromTo(
+    section,
+    {
+      backgroundPosition: () =>
+        i ? `50% ${-window.innerHeight * getRatio(section)}px` : "50% 0px",
+    },
+    {
+      backgroundPosition: () =>
+        `50% ${window.innerHeight * (1 - getRatio(section))}px`,
+      ease: "none",
+      scrollTrigger: {
+        trigger: section,
+        start: () => (i ? "top bottom" : "top top"),
+        end: "bottom top",
+        scrub: 0.4,
+        invalidateOnRefresh: true, // to make it responsive
+      },
     }
-  });
-
+  );
 });
 
 gsap.to(".med-staff .image", {
-    scrollTrigger : {
-        trigger: ".med-staff",
-        start: 'top 50%',
-        scrub: true,
-        end: 'bottom 50%',
-    },
-    x: 0,
-    opacity: 1
+  scrollTrigger: {
+    trigger: ".med-staff",
+    start: "top 50%",
+    scrub: true,
+    end: "bottom 50%",
+  },
+  x: 0,
+  opacity: 1,
 });
 gsap.to(".med-staff .copy", {
-    scrollTrigger : {
-        trigger: ".med-staff",
-        start: 'top 50%',
-        scrub: true,
-        end: 'bottom 50%',
-    },
-    x: "-50%",
+  scrollTrigger: {
+    trigger: ".med-staff",
+    start: "top 50%",
+    scrub: true,
+    end: "bottom 50%",
+  },
+  x: "-50%",
 });
